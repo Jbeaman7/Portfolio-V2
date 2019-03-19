1ebaa72c2d67914fd639c20fcbd72218a37782d7
Jordan Beaman <Jordanbeaman94@gmail.com>
2019-03-19 14:39:23 -0500

    Initial Commit

---
+++ src/resumeData.js
@@ -0,0 +1,105 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Jordan Beaman",
+    "role": "Web Developer/Designer",
+    "roleDescription": " I love learning new technologies, and am always looking to expand my knowledge of development. In my free time you can find me playing games, reading a book, checking out a new brewery, or attending sporting events with my friends. Feel free to reach out to me any time!",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/jordan-beaman",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Jbeaman7",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"My name is Jordan Beaman. I'm a recent graduate of the University of Kansas Coding Bootcamp who's always striving to find innovative yet practical ways to solve any problem that comes my way. I’m known for getting the job done, on time, and above expectations.",
+    "address":"Kansas City, Missouri",
+    "website":"(913) 256-8942",
+    "education":[
+      {
+        "UniversityName":"University of Kansas",
+        "specialization":"University of Kansas Full Stack Flex Coding Bootcamp",
+        "MonthOfPassing":"February",
+        "YearOfPassing":"2019",
+      },
+      {
+        "UniversityName":"Leavenworth High School",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2012",
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"Vision Source",
+        "specialization":"Optometric Technician",
+        "MonthOfLeaving":"Mar",
+        "YearOfLeaving":"2018-Present",
+      },
+      {
+        "CompanyName":"Kansas CIty Airport Marriott",
+        "specialization":"Guest Service Representative",
+        "MonthOfLeaving":"Mar",
+        "YearOfLeaving":"2018",
+      },
+      {
+        "CompanyName":"Saint George Hotel",
+        "specialization":"Guest Service Representative/Bartender",
+        "MonthOfLeaving":"Oct",
+        "YearOfLeaving":"2017",
+      },
+    ],
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"jQuery"
+      },
+      {
+        "skillname":"git"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Backroads Art",
+        "description":"This is a site made for a local art shop in Weston, MO made using Bootstrap 4",
+        "imgurl":"images/portfolio/modals/curator.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/coffee.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Jordan Beaman recently assisted us by redesigning our website.  We were extremely pleased with the end result and with the whole process.  Jordan not only brought our website up to date and made it mobile friendly but he also did a great job of improving the overall appearance.  He added some touches that we would not have thought of such as a button where people could call immediately.  We are not at all tech-savvy so Jordan’s expertise was greatly appreciated.  Jordan is extremely bright and outgoing and is just a pleasure to work with.",
+        "name":"Steve and Denise Jarrett - Back Roads Art"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
